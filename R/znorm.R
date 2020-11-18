45ff202545e08de6bd3f80ab4c655f4226e72784
zhaoshengEE <jerryli20160808@gmail.com>
2020-11-17 23:23:34 -0800

    Update R function

--- R/znorm.R
+++ R/znorm.R
@@ -1,3 +1,16 @@
+#' Normalizing data vector
+#'
+#' @param x Numeric vector.
+#' @param removeNA A logical value indicating whether NA values should be removed, with default value of TRUE.
+#' @param ...
+#'
+#' @return
+#' A vector containing the normalized input data vector
+#' @export
+#'
+#' @examples
+#' znorm(c(16, 9, 13, 5))
+
 znorm <- function(x, removeNA = TRUE, ...){
   mean <- mean(x, na.rm = removeNA, ...)
   sd <- sd(x, na.rm = removeNA)
