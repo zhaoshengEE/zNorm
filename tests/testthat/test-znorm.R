4abb1235580a2723169090580e0960c228a4cbcc
zhaoshengEE <jerryli20160808@gmail.com>
2020-11-18 00:42:13 -0800

    Update test code

--- tests/testthat/test-znorm.R
+++ tests/testthat/test-znorm.R
@@ -1,6 +1,13 @@
-test_that("Formal test", {
+test_that("Basic test", {
+  # Simple test with a simple numeric vector as input
   expect_equal(znorm(c(1,5,9,10)), (c(1,5,9,10) - mean(c(1,5,9,10))) / sd(c(1,5,9,10)))
+
+  # Test whether the default value of removeNA is True or not
   expect_equal(znorm(c(2,5,8,NA)), (c(2,5,8,NA) - mean(c(2,5,8,NA),na.rm = TRUE)) / sd(c(2,5,8,NA),na.rm = TRUE))
+
+  # Test the ... input of znorm function
   expect_equal(znorm(c(1,5,9,10),trim = 0.2), (c(1,5,9,10) - mean(c(1,5,9,10),trim = 0.2)) / sd(c(1,5,9,10)))
+
+  # Test the znorm function with non-numeric vector as input, which is expected to pop out warnings
   expect_error(znorm(c('abc','efg','qwr')))
 })
